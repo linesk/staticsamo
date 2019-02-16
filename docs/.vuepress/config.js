add9924f319ee08d9d987de28ea83282024e7f69
Ratchapoom <Ratchapoom.k@kkumail.com>
2019-02-16 23:46:21 +0700

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'MDKKU SAMO 62',
+    description: 'Energy of the Medicine',
+    themeConfig: {
+        nav: [{
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'About us',
+                link: '/aboutus/'
+            },
+        ],
+        sidebar: 'auto'
+    }
+}
