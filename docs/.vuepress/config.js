6eb4fd0bd0d5df1946a3554a57ee8b4d183f226a
Ratchapoom <Ratchapoom.k@kkumail.com>
2019-02-17 17:10:24 +0700

    .

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,45 @@
 module.exports = {
     title: 'MDKKU SAMO 62',
     description: 'Energy of the Medicine',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/images/logo.png'
+        }]
+    ],
+    plugins: [
+        '@vuepress/back-to-top',
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': '' // UA-00000000-0
+            }
+        ]
+    ],
     themeConfig: {
+        logo: '/images/logo.png',
         nav: [{
                 text: 'Home',
                 link: '/'
             },
             {
-                text: 'About us',
+                text: 'About Us',
                 link: '/aboutus/'
             },
+            {
+                text: 'Contact Us',
+                link: '/contactus.html'
+            },
         ],
-        sidebar: 'auto'
+        sidebar: {
+            '/aboutus/': [
+                '',
+                'policy',
+                'organization',
+            ],
+        },
+        lastUpdated: 'Last Updated',
     },
-    theme: 'cool',
     base: '/staticsamo/',
     configureWebpack: {
         resolve: {
@@ -21,5 +47,6 @@ module.exports = {
                 '@alias': 'path/to/some/dir'
             }
         }
-    }
+    },
+    serviceWorker: true
 }
